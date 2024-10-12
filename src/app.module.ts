153de877c1fee660ba3cbb69ac6dfdc607fd2746
ee227060 <ee227060@meiji.ac.jp>
2024-10-12 16:29:41 +0900

    findAllの追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'database.sqlite',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true
+  }),
+  TodoModule]
 })
 export class AppModule {}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TodoService } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
     constructor(private readonly todoService: TodoService){}
 
+    @Get()
+    findAll(){
+        return 'get all task';
+    }
     
 }
